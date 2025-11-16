a88a934933fb7ce26b39bf836bfc584ffd7aea55
AshleeGarcia <ashleegarcia.dev@gmail.com>
2025-11-15 19:15:54 -0500

    feat: crear ProductoController para listar productos

---
+++ backend/src/main/java/com/motorpoint/backend/controller/ProductoController.java
@@ -0,0 +1,23 @@
+package com.motorpoint.backend.controller;
+
+import com.motorpoint.backend.entity.Producto;
+import com.motorpoint.backend.service.ProductoService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+@CrossOrigin(origins = "*")
+public class ProductoController {
+
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @GetMapping
+    public List<Producto> listar() {
+        return productoService.listarProductos();
+    }
+}
