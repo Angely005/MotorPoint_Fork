f1c6d09e43bb37d995bc6c2e0bdae3d617afc414
Angely <angelycorahua2005@gmail.com>
2025-11-19 23:06:38 -0500

    refactor: Actualizar RolRepository para usar Long como ID y agregar un método para buscar roles por nombre.

--- backend/src/main/java/com/motorpoint/backend/repository/RolRepository.java
+++ backend/src/main/java/com/motorpoint/backend/repository/RolRepository.java
@@ -1,7 +1,11 @@
 package com.motorpoint.backend.repository;
 
 import com.motorpoint.backend.entity.Rol;
+import com.motorpoint.backend.entity.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RolRepository extends JpaRepository<Rol, Integer> {
+import java.util.Optional;
+
+public interface RolRepository extends JpaRepository<Rol, Long> {
+    Optional<Rol> findByNombre(RoleName nombre);
 }
